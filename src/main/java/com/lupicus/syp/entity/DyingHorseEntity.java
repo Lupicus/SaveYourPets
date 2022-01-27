b61793fed17139ba5b9cc7cb61e3a8d72549031a
Lupicus <54924432+Lupicus@users.noreply.github.com>
2022-01-26 23:16:52 -0600

    Give credit and let poisonous potato kill pet

--- src/main/java/com/lupicus/syp/entity/DyingHorseEntity.java
+++ src/main/java/com/lupicus/syp/entity/DyingHorseEntity.java
@@ -20,24 +20,27 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
 public abstract class DyingHorseEntity extends AbstractHorse implements IDying
 {
 	protected long woundedTime;
 	protected int woundedTicks;
+	protected UUID killerUUID;
+	protected UUID scoreUUID;
 
 	protected DyingHorseEntity(EntityType<? extends AbstractHorse> type, Level worldIn) {
 		super(type, worldIn);
@@ -54,6 +57,10 @@ public abstract class DyingHorseEntity extends AbstractHorse implements IDying
 				woundedTicks -= compound.getInt("WoundedTicks");
 			else
 				woundedTicks -= (int) (level.getGameTime() - woundedTime);
+			if (compound.hasUUID("sypKiller"))
+				killerUUID = compound.getUUID("sypKiller");
+			if (compound.hasUUID("sypScore"))
+				scoreUUID = compound.getUUID("sypScore");
 			entityData.set(DATA_POSE, Pose.DYING);
 			setHealth(0.0F);
 			deathTime = 20;
@@ -67,6 +74,10 @@ public abstract class DyingHorseEntity extends AbstractHorse implements IDying
 		{
 			compound.putLong("WoundedTime", woundedTime);
 			compound.putInt("WoundedTicks", tickCount - woundedTicks);
+			if (killerUUID != null)
+				compound.putUUID("sypKiller", killerUUID);
+			if (scoreUUID != null)
+				compound.putUUID("sypScore", scoreUUID);
 		}
 	}
 
@@ -104,9 +115,15 @@ public abstract class DyingHorseEntity extends AbstractHorse implements IDying
 				player.sendMessage(msg, player.getUUID());
 			}
 			unRide();
-			this.entityData.set(DATA_POSE, Pose.DYING);
+			entityData.set(DATA_POSE, Pose.DYING);
 			woundedTime = level.getGameTime();
 			woundedTicks = tickCount;
+			Entity entity = cause.getEntity();
+			if (entity instanceof ServerPlayer)
+				killerUUID = entity.getUUID();
+			LivingEntity le = getKillCredit();
+			if (le instanceof ServerPlayer)
+				scoreUUID = le.getUUID();
 			setYRot(yBodyRot);
 			yHeadRot = yBodyRot;
 			setXRot(0.0F);
@@ -129,23 +146,38 @@ public abstract class DyingHorseEntity extends AbstractHorse implements IDying
 		}
 		else if (!level.isClientSide)
 		{
-		    int time = (MyConfig.useWorldTicks) ? (int) (level.getGameTime() - woundedTime) : tickCount - woundedTicks;
-		    if (time < MyConfig.deathTimer)
-		    	return;
+			int time = (MyConfig.useWorldTicks) ? (int) (level.getGameTime() - woundedTime) : tickCount - woundedTicks;
+			if (time < MyConfig.deathTimer)
+				return;
 			if (MyConfig.autoHeal)
 			{
 				cureEntity(ModItems.GOLDEN_PET_BANDAGE);
 			}
 			else
 			{
-				super.die(DamageSource.GENERIC);
-				level.broadcastEntityEvent(this, (byte) 60);
-				remove(RemovalReason.KILLED);
+				killEntity();
 			}
 		}
 	}
 
-	@OnlyIn(Dist.CLIENT)
+	void killEntity()
+	{
+		if (scoreUUID != null)
+		{
+			lastHurtByPlayer = level.getPlayerByUUID(scoreUUID);
+		}
+		DamageSource ds = DamageSource.GENERIC;
+		if (killerUUID != null)
+		{
+			Player aPlayer = level.getPlayerByUUID(killerUUID);
+			if (aPlayer != null)
+				ds = DamageSource.playerAttack(aPlayer);
+		}
+		super.die(ds);
+		deathTime = 19;
+		super.tickDeath();
+	}
+
 	@Override
 	public void handleEntityEvent(byte id)
 	{
@@ -153,11 +185,6 @@ public abstract class DyingHorseEntity extends AbstractHorse implements IDying
 			cureEntity(null);
 		else {
 			super.handleEntityEvent(id);
-			if (id == 3 && isTamed())
-			{
-				deathTime = 19;
-				super.tickDeath();
-			}
 		}
 	}
 
@@ -177,12 +204,24 @@ public abstract class DyingHorseEntity extends AbstractHorse implements IDying
 		if (item == ModItems.PET_BANDAGE || item == ModItems.GOLDEN_PET_BANDAGE)
 		{
 			player.awardStat(Stats.ITEM_USED.get(item));
-            if (!player.getAbilities().instabuild) {
-            	itemstack.shrink(1);
-            }
+			if (!player.getAbilities().instabuild) {
+				itemstack.shrink(1);
+			}
 			if (player instanceof ServerPlayer) {
 				ModTriggers.SAVE_PET.trigger((ServerPlayer) player, this);
 				cureEntity(item);
+			}
+			return InteractionResult.sidedSuccess(level.isClientSide);
+		}
+		else if (item == Items.POISONOUS_POTATO)
+		{
+			player.awardStat(Stats.ITEM_USED.get(item));
+			if (!player.getAbilities().instabuild) {
+				itemstack.shrink(1);
+			}
+			if (player instanceof ServerPlayer) {
+				killerUUID = player.getUUID();
+				killEntity();
 			}
 			return InteractionResult.sidedSuccess(level.isClientSide);
 		}
@@ -197,18 +236,20 @@ public abstract class DyingHorseEntity extends AbstractHorse implements IDying
 	{
 		if (level.isClientSide)
 		{
-			for (int i = 0; i < 10; ++i) {
+			for (int i = 0; i < 7; ++i) {
 				double d0 = this.random.nextGaussian() * 0.02D;
 				double d1 = this.random.nextGaussian() * 0.02D;
 				double d2 = this.random.nextGaussian() * 0.02D;
-				this.level.addParticle(ParticleTypes.HEART, this.getRandomX(1.0D), this.getRandomY(),
+				this.level.addParticle(ParticleTypes.HEART, this.getRandomX(1.0D), this.getRandomY() + 0.5D,
 						this.getRandomZ(1.0D), d0, d1, d2);
 			}
 		}
 		else
 			level.broadcastEntityEvent(this, (byte) 101);
 		setDeltaMovement(Vec3.ZERO);
-		this.entityData.set(DATA_POSE, Pose.STANDING);
+		entityData.set(DATA_POSE, Pose.STANDING);
+		killerUUID = null;
+		scoreUUID = null;
 		setHealth(1.0F);
 		deathTime = 0;
 		if (!level.isClientSide && isInWall())
