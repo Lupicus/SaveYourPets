b91697ca956df495af340c3235c4870068a6e905
Lupicus <54924432+Lupicus@users.noreply.github.com>
2023-12-14 17:01:03 -0600

    Update to 1.20.4

--- src/main/java/com/lupicus/syp/advancements/criterion/SavePetTrigger.java
+++ src/main/java/com/lupicus/syp/advancements/criterion/SavePetTrigger.java
@@ -2,25 +2,25 @@ package com.lupicus.syp.advancements.criterion;
 
 import java.util.Optional;
 
-import com.google.gson.JsonObject;
 import com.lupicus.syp.advancements.ModTriggers;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import net.minecraft.advancements.Criterion;
-import net.minecraft.advancements.critereon.AbstractCriterionTriggerInstance;
 import net.minecraft.advancements.critereon.ContextAwarePredicate;
-import net.minecraft.advancements.critereon.DeserializationContext;
+import net.minecraft.advancements.critereon.CriterionValidator;
 import net.minecraft.advancements.critereon.EntityPredicate;
 import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.ExtraCodecs;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.level.storage.loot.LootContext;
 
-public class SavePetTrigger extends SimpleCriterionTrigger<SavePetTrigger.Instance>
+public class SavePetTrigger extends SimpleCriterionTrigger<SavePetTrigger.TriggerInstance>
 {
 	@Override
-	public SavePetTrigger.Instance createInstance(JsonObject json, Optional<ContextAwarePredicate> player, DeserializationContext ctx) {
-		Optional<ContextAwarePredicate> entitypredicate = EntityPredicate.fromJson(json, "entity", ctx);
-		return new SavePetTrigger.Instance(player, entitypredicate);
+	public Codec<SavePetTrigger.TriggerInstance> codec() {
+		return SavePetTrigger.TriggerInstance.CODEC;
 	}
 
 	public void trigger(ServerPlayer player, Animal entity) {
@@ -30,20 +30,17 @@ public class SavePetTrigger extends SimpleCriterionTrigger<SavePetTrigger.Instan
 		});
 	}
 
-	public static class Instance extends AbstractCriterionTriggerInstance {
-		private final Optional<ContextAwarePredicate> entity;
+	public static record TriggerInstance(Optional<ContextAwarePredicate> player, Optional<ContextAwarePredicate> entity) implements SimpleCriterionTrigger.SimpleInstance {
+		public static final Codec<SavePetTrigger.TriggerInstance> CODEC = RecordCodecBuilder.create((inst) -> {
+			return inst.group(ExtraCodecs.strictOptionalField(EntityPredicate.ADVANCEMENT_CODEC, "player").forGetter(SavePetTrigger.TriggerInstance::player), ExtraCodecs.strictOptionalField(EntityPredicate.ADVANCEMENT_CODEC, "entity").forGetter(SavePetTrigger.TriggerInstance::entity)).apply(inst, SavePetTrigger.TriggerInstance::new);
+		});
 
-		public Instance(Optional<ContextAwarePredicate> player, Optional<ContextAwarePredicate> entity) {
-			super(player);
-			this.entity = entity;
+		public static Criterion<SavePetTrigger.TriggerInstance> savePet() {
+			return ModTriggers.SAVE_PET.createCriterion(new SavePetTrigger.TriggerInstance(Optional.empty(), Optional.empty()));
 		}
 
-		public static Criterion<SavePetTrigger.Instance> savePet() {
-			return ModTriggers.SAVE_PET.createCriterion(new SavePetTrigger.Instance(Optional.empty(), Optional.empty()));
-		}
-
-		public static Criterion<SavePetTrigger.Instance> savePet(EntityPredicate.Builder predicate) {
-			return ModTriggers.SAVE_PET.createCriterion(new SavePetTrigger.Instance(Optional.empty(), Optional.of(EntityPredicate.wrap(predicate))));
+		public static Criterion<SavePetTrigger.TriggerInstance> savePet(EntityPredicate.Builder predicate) {
+			return ModTriggers.SAVE_PET.createCriterion(new SavePetTrigger.TriggerInstance(Optional.empty(), Optional.of(EntityPredicate.wrap(predicate))));
 		}
 
 		public boolean matches(LootContext lootcontext) {
@@ -51,12 +48,13 @@ public class SavePetTrigger extends SimpleCriterionTrigger<SavePetTrigger.Instan
 		}
 
 		@Override
-		public JsonObject serializeToJson() {
-			JsonObject jsonobject = super.serializeToJson();
-			entity.ifPresent((c) -> {
-				jsonobject.add("entity", c.toJson());
-			});
-			return jsonobject;
+		public void validate(CriterionValidator p_309538_) {
+			SimpleCriterionTrigger.SimpleInstance.super.validate(p_309538_);
+			p_309538_.validateEntity(this.entity, ".entity");
+		}
+
+		public Optional<ContextAwarePredicate> player() {
+			return this.player;
 		}
 	}
 }
